e6545af62370bb9a57f236cffc0208a38256b779
zhuangjinyu <396572591@qq.com>
2020-07-28 11:12:25 +0800

    2020/7/27

--- cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
+++ cloud-provider-hystrix-payment8001/src/main/java/com/atguigu/springcloud/service/PaymentService.java
@@ -7,4 +7,6 @@ public interface PaymentService {
     public String paymentInfo_OK(Integer id);
 
     public String paymentInfo_TimeOut(Integer id);
+
+    public String paymentCircuitBreaker( Integer id);
 }
